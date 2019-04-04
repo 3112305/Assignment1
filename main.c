94fed0502a80d75692d710d2a157e2509bf60935
Oliver Teiwsen <oliver.teiwsen@uon.edu.au>
2019-04-04 06:54:19 +0000

    first attempt at scanning and printing from a file

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 
 int main(){
 
-  //test
-  //test
-  //test
+  
+  char test[14]; // initialise array of char variables, with the same number of
+                 // elements as the amount of characters in the Test_input2 file
+  
+  int i; // indexing integer for scanning/printing the characters from Test_input2 file
+    
+   
+  
+  for (i = 0; i <= 14; i++){ // 
+      scanf("%c", &test[i]);
+      printf("%c", test[i]);
+  }
+  
   return 0;
 }
