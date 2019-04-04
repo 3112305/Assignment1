8c531daf9e03d19f106fd4675598b1a4c0c5da9e
Oliver Teiwsen <oliver.teiwsen@uon.edu.au>
2019-04-04 07:06:49 +0000

    First attempt at a rotation encryption, seems to work ok, still reads scanf from Test_input2 file

--- main.c
+++ main.c
@@ -10,8 +10,14 @@ int main(){
     
    
   
-  for (i = 0; i <= 14; i++){ // 
+  for (i = 0; i <= 14; i++){ // for loop scans and prints the letters of the file one at a time
       scanf("%c", &test[i]);
+      test[i] = test[i]+1; // this adds 1 to the ascii value of each letter before printing.
+                           // the number 1 would be used as the key of a rotation cypher,
+                           // and could be replaced with a variable eg... int cypherKey = 1;
+                           // An if statement could be used to check whether the character read is
+                           // in the bounds of A-Z, a-z and not add any ascii value if it is a space
+                           // or any other non alphabet character.
       printf("%c", test[i]);
   }
   
