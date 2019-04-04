7b60fae589183199f3600712df37669c6a2fae96
Oliver Teiwsen <oliver.teiwsen@uon.edu.au>
2019-04-04 07:34:45 +0000

    edited to wrap around if the key pushes letters past Z,z. changed key to 7, added more text to input test file

--- main.c
+++ main.c
@@ -3,25 +3,34 @@
 int main(){
 
   
-  char test[14]; // initialise array of char variables, with the same number of
+  char test[41]; // initialise array of char variables, with the same number of
                  // elements as the amount of characters in the Test_input2 file
   
   int i; // indexing integer for scanning/printing the characters from Test_input2 file
     
    
   
-  for (i = 0; i <= 14; i++){ // for loop scans and prints the letters of the file one at a time
+  for (i = 0; i <= 41; i++){ // for loop scans and prints the letters of the file one at a time
       scanf("%c", &test[i]);
       
-      if(((test[i]>=65)&&(test[i]<=90))||((test[i]>=97)&&(test[i]<=122))){// checks if the character read is an alphabet character,
-                                                                          // only adds 1 if it is, then moves onto printing.
+      if((test[i]>=65)&&(test[i]<=90)){// checks if the character read is an alphabet character,
+                                        // only adds 1 if it is, then moves onto printing.
       
-          test[i] = test[i]+1; // this adds 1 to the ascii value of each letter before printing.
-                               // the number 1 would be used as the key of a rotation cypher,
-                               // and could be replaced with a variable eg... int cypherKey = 1;
-                               // The next step would be to make it so that if any letter is used
-                               // that gets pushed out of the a-z,A-Z boundary (eg z+1 = ???),
-                               // that letter should wrap back around to the start of the alphabet.
+         test[i] = test[i]+7; // this adds 7 to the ascii value of each letter before printing.
+                              // the number 7 would be used as the key of a rotation cypher,
+                              // and could be replaced with a variable eg... int cypherKey = 1;
+                              
+         if(test[i]>90){
+            test[i] = test[i]-26;
+         }                     
+      }
+      
+      if((test[i]>=97)&&(test[i]<=122)){//same as above if statement, but for A-Z range
+         test[i] = test[i]+7;
+         
+         if(test[i]>122){
+            test[i] = test[i]-26;
+         }   
       }
 
       printf("%c", test[i]);
