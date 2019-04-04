3c84130d802f527c18e2709504fd29e76e2f70e1
Oliver Teiwsen <oliver.teiwsen@uon.edu.au>
2019-04-04 07:19:22 +0000

    rotation cypher now skips non-alphabet characters

--- main.c
+++ main.c
@@ -12,12 +12,18 @@ int main(){
   
   for (i = 0; i <= 14; i++){ // for loop scans and prints the letters of the file one at a time
       scanf("%c", &test[i]);
-      test[i] = test[i]+1; // this adds 1 to the ascii value of each letter before printing.
-                           // the number 1 would be used as the key of a rotation cypher,
-                           // and could be replaced with a variable eg... int cypherKey = 1;
-                           // An if statement could be used to check whether the character read is
-                           // in the bounds of A-Z, a-z and not add any ascii value if it is a space
-                           // or any other non alphabet character.
+      
+      if(((test[i]>=65)&&(test[i]<=90))||((test[i]>=97)&&(test[i]<=122))){// checks if the character read is an alphabet character,
+                                                                          // only adds 1 if it is, then moves onto printing.
+      
+          test[i] = test[i]+1; // this adds 1 to the ascii value of each letter before printing.
+                               // the number 1 would be used as the key of a rotation cypher,
+                               // and could be replaced with a variable eg... int cypherKey = 1;
+                               // The next step would be to make it so that if any letter is used
+                               // that gets pushed out of the a-z,A-Z boundary (eg z+1 = ???),
+                               // that letter should wrap back around to the start of the alphabet.
+      }
+
       printf("%c", test[i]);
   }
   
