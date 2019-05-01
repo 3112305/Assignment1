38ba348cca8d476f66855dce935298a040381ca7
Oliver Teiwsen <oliver.teiwsen@uon.edu.au>
2019-05-01 00:38:59 +0000

    Tidying up after restructure

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ char getSubKey(void); // gets key from file subCiphKey.txt, returns key
 char substKey(char, char);//substitution function, pass message, key, return ciphered message
 char substNoKey(char);//substitution cipher function prototype
 void fileWrite(char, char); // function for file writing, pass message and write file, no returns
-char printMessage(char); // reads message file and prints contents send filename, return message
+void printMessage(char); // reads message file and prints contents send filename, return message
 
     
 // sub and rotation cipher below, attempting a menu system etc now
@@ -33,10 +33,10 @@ int main(){
     int progSelection1;
     int progSelection2;
     //int progSelection3;
-    char filename;
-    char message;
+    char filename[30];
+    char message[10];
     int rotKey;
-    char subKey;
+    char subKey[26];
     
     
     while((progSelection1!=1)&&(progSelection1!=2)){
@@ -64,24 +64,24 @@ int main(){
                     
        if(progSelection2 == 1){
           printf("\n*****\nRotation cipher message selected.\n");
-          filename = "rotationCipher.txt";//set filename
-          message = fileRead(filename);// run fileRead function to read messagefrom file
+          filename[0] = "RotationCipher.txt";//set filename
+          message[0] = fileRead(filename);// run fileRead function to read messagefrom file
           rotKey = getRotKey(); // then get rotation key from file // run function to get the rotation key
-          message = rotateKey(message,rotKey); // then rotate message with key
+          message[0] = rotateKey(message,rotKey); // then rotate message with key
           fileWrite(filename, message); // then write rotated message to file
-          printMessage(filename) // open and print contents of file to confirm
+          printMessage(filename); // open and print contents of file to confirm
        }
        
        //IF SUBSTITUTION CIPHER IS SELECTED
        
        if(progSelection2 == 2){
           printf("\n*****\nSubstitution cipher message selected.\n");
-          filename = "substitutionCipher.txt";//set filename
-          message = fileRead(filename);// run fileRead function to read messagefrom file
-          subKey = getSubKey(); // then get substitution key from file
-          message = substKey(message,subKey); // then substitute message with key
+          filename[] = "substitutionCipher.txt";//set filename
+          message[] = fileRead(filename);// run fileRead function to read messagefrom file
+          subKey[] = getSubKey(); // then get substitution key from file
+          message[] = substKey(message,subKey); // then substitute message with key
           fileWrite(filename, message); // then write substituted message to file
-          printMessage(filename) // open and print contents of file to confirm
+          printMessage(filename); // open and print contents of file to confirm
        }
 
        // once the above code is done, the program should skip the remaining code as progSelection is 1,  
@@ -107,16 +107,16 @@ int main(){
           
           printf("\n*****\nRotation decipher with key selected.\n");
           
-          filename = "rotationDecipher.txt";//set filename
-          message = fileRead(filename);// run fileRead function to read messagefrom file
+          filename[] = "rotationDecipher.txt";//set filename
+          message[] = fileRead(filename);// run fileRead function to read messagefrom file
           
           rotKey = getRotKey(); // then get rotation key from file // run function to get the rotation key
 /////////////VVVVVVVVVV????????
-          rotKey = (26 + rotKey)// FIX MINUS WRAPAROUND what do you do here to fix it?? 
-          message = rotateKey(message,rotKey); // then rotate message with key
+          rotKey = (26 + rotKey);// FIX MINUS WRAPAROUND what do you do here to fix it?? 
+          message[] = rotateKey(message,rotKey); // then rotate message with key
           
           fileWrite(filename, message); // then write rotated message to file
-          printMessage(filename) // open and print contents of file to confirm
+          printMessage(filename); // open and print contents of file to confirm
        
        }
        
@@ -126,13 +126,13 @@ int main(){
           //RotDeciphNoKey
           printf("\n*****\nRotation decipher without key selected.\n");
           
-          filename = "rotationDecipher.txt";//set filename
+          filename[] = "rotationDecipher.txt";//set filename
           
-          message = fileRead(filename);// run fileRead function to read messagefrom file
-          message = rotateNoKey(message); // then rotate message with key
+          message[] = fileRead(filename);// run fileRead function to read messagefrom file
+          message[] = rotateNoKey(message); // then rotate message with key
           
           fileWrite(filename, message); // then write rotated message to file
-          printMessage(filename) // open and print contents of file to confirm
+          printMessage(filename); // open and print contents of file to confirm
        
        }
        
@@ -142,27 +142,27 @@ int main(){
           //SubDeciphKey
           printf("\n*****\nSubstitution decipher with key selected.\n");
           
-          filename = "subtitutionDecipher.txt";//set filename
+          filename[] = "subtitutionDecipher.txt";//set filename
           
-          message = fileRead(filename);// run fileRead function to read message from file
-          subKey = getSubKey(); // then get sub key from file // run function to get the rotation key
-          message = substKey(message,subKey); // then rotate message with key
+          message[] = fileRead(filename);// run fileRead function to read message from file
+          subKey[] = getSubKey(); // then get sub key from file // run function to get the rotation key
+          message[] = substKey(message,subKey); // then rotate message with key
           
           fileWrite(filename, message); // then write rotated message to file
-          printMessage(filename) // open and print contents of file to confirm
+          printMessage(filename); // open and print contents of file to confirm
        
        }
        if(progSelection2 == 4){
            //SubDeciphNoKey
           printf("\n*****\nSubstitution decipher without key selected.\n");
           
-          filename = "subtitutionDecipher.txt";//set filename
+          filename[] = "subtitutionDecipher.txt";//set filename
           
-          message = fileRead(filename);// run fileRead function to read message from file
-          message = substNoKey(message); // then rotate message with key
+          message[] = fileRead(filename);// run fileRead function to read message from file
+          message[] = substNoKey(message); // then rotate message with key
           
           fileWrite(filename, message); // then write rotated message to file
-          printMessage(filename) // open and print contents of file to confirm
+          printMessage(filename); // open and print contents of file to confirm
        
        }
         
@@ -234,12 +234,17 @@ char rotateKey(char message, int key){// rotational function with key, pass mess
          }
       }
    }   
+   return message;
 }
 
 
-char rotateNoKey(char message){...}// rotational function without key, pass message, return rotated message, write key to stdout
+char rotateNoKey(char message){// rotational function without key, pass message, return rotated message, write key to stdout
  // can use statistical analysis and ask user if it looks right,
  // bash at it with statistical keys until its right
+   char key;
+   return key;
+}
+
 
 char getSubKey(void){// gets key from file subCiphKey.txt, returns key
    char key;
@@ -260,38 +265,9 @@ char substKey(char message, char key){//substitution function, pass message, key
    // tidy it up and put it here
    //
    //
-   // key array for substitution, if program finds an A, it will subst it with the [0] element of this array, B will become [1] element, and so on
-  //              'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-  //char key[26] = {'k', 'e', 'i', 'D', 'u', 't', 'v', 'h', 'p', 's', 'n', 'c', 'f', 'a', 'm', 'w', 'g', 'd', 'S', 'T', 'x', 'y', 'b', 'r', 'o', 'l'};
-  //char test[216]; // initialise array of char variables, with the same number of
-                 // elements as the amount of characters in the Test_input2 file
-  
-  //have message and subst key at this point in char message and char key
-  
-  //most common words and letters
-  char comWords[100] = {'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us'};
-  char comLetters[26] = {'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'};
-  char countLetter[27];
-  
-  
-  // count repeated letters(only) for message[i] ---> i = 0 to end
-  // if letter is a, index a etc, print most common to check
-  
-  
-  for(i=0, i<1000,i++){  // scan through each letter of the message
-     while(j=0; j<=25; j++){ // check each letter against the list of common letters
-         
-        if(message[i]==comLetter[j]){
-            countLetter[j] = countletter[j] + 1; counts how many times each letter appears
-        }
-
-     }
-     if(message[i] == 32){ // 32 is ASCII space
-         countLetter[26] = countLetter[26] + 1; // count spaces
-     }
    
-  
-  }
+  //have message and subst key at this point in char message and char key
+
   
   int i; // indexing integer for scanning/printing the characters from Test_input2 file
   int k; // indexing integer for letter identification
@@ -317,18 +293,16 @@ char substKey(char message, char key){//substitution function, pass message, key
           printf("%c", test[i]);
       } 
       
-      
-      //printf("%c", test[i]);  
-                 
+                       
   }
   
-  return 0;
+  return message;
 }
 
 char substNoKey(char message){//substitution cipher function prototype
 
    //most common words and letters
-   char comWords[100] = {'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us'};
+   //char comWords[100] = {'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us'};
    char comLetters[26] = {'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'}; 
    char countLetter[27];
   
@@ -350,6 +324,7 @@ char substNoKey(char message){//substitution cipher function prototype
       }
    
       printf("do i need to print anything here?\n");// print results, word count (ie space count + 1)
+      return message;
       
    }
    
@@ -380,14 +355,14 @@ void fileWrite(char filename, char message){// function for file writing, pass m
 
 
 //
-char printMessage(char filename){
+void printMessage(char filename){
     
    char message;
    int i = 0;
    
    FILE *messagePointer = fopen(filename,"r");
    
-   while (!feof(messagePointer))
+   while (!feof(messagePointer)){
        fscanf(messagePointer,"%c", &message[i]);
        printf("c", message[i]);
        i++;
@@ -404,7 +379,6 @@ char printMessage(char filename){
 
 
  
-
 
 
 
